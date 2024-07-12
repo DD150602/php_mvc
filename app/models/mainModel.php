696e0ef3602bfde9d072975cdd0e5107cfdb4bca
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-07-12 16:57:02 -0500

    ✨ create methods to delete and select also amethod to make pagination

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -35,11 +35,13 @@ class mainModel
 
   public function limpiarCadena($cadena)
   {
+    // texto que no esta permitido dentro de las cadenas de texto
     $palabras = ["<script>", "</script>", "<script src", "<script type=", "SELECT * FROM", "SELECT ", " SELECT ", "DELETE FROM", "INSERT INTO", "DROP TABLE", "DROP DATABASE", "TRUNCATE TABLE", "SHOW TABLES", "SHOW DATABASES", "<?php", "?>", "--", "^", "<", ">", "==", "=", ";", "::"];
     $cadena = trim($cadena);
     $cadena = stripslashes($cadena);
 
     foreach ($palabras as $palabra) {
+      // quita de la cadena la palabra que se le pase por parametro
       $cadena = str_ireplace($palabra, "", $cadena);
     }
     $cadena = trim($cadena);
@@ -49,6 +51,7 @@ class mainModel
 
   protected function verificarDatos($filtro, $cadena)
   {
+    // preg_match realiza una comparacion con una expresion regular
     if (preg_match("/^" . $filtro . "/", $cadena)) {
       return false;
     } else {
@@ -64,6 +67,7 @@ class mainModel
       if ($C >= 1) {
         $query .= ", ";
       }
+      // campo nombre es el nombre de la columna de la base de datos
       $query .= $clave['campo_nombre'];
       $C++;
     }
@@ -73,16 +77,115 @@ class mainModel
       if ($C >= 1) {
         $query .= ", ";
       }
+      // campo marcador es el marcador de la que identifica los campos que se traen del formulario
       $query .= $clave['campo_marcador'];
       $C++;
     }
     $query .= ")";
+    // el metodo prepare es de la clase pdo para preparar una consilta sql
+    // la consulta contiene marcadores de parametros
     $sql = $this->conectar()->prepare($query);
 
     foreach ($datos as $clave) {
+      // bindParam es para vincular un parametro con una variable especifica
       $sql->bindParam($clave['campo_marcador'], $clave['campo_valor']);
     }
     $sql->execute();
     return $sql;
+  }
+
+  public function seleccionarDatos($tipo, $tabla, $campo, $id)
+  {
+    $tipo = $this->limpiarCadena($tipo);
+    $tabla = $this->limpiarCadena($tabla);
+    $campo = $this->limpiarCadena($campo);
+    $id = $this->limpiarCadena($id);
+
+    if ($tipo == "Unico") {
+      $sql = $this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo = :ID");
+      $sql->bindParam(":ID", $id);
+    } elseif ($tipo == "Normal") {
+      $sql = $this->conectar()->prepare("SELECT $campo FROM $tabla");
+    }
+    $sql->execute();
+    return $sql;
+  }
+  protected function actualizarDatos($tabla, $datos, $condicion)
+  {
+    $query = "UPDATE $tabla SET ";
+    $C = 0;
+    foreach ($datos as $clave) {
+      if ($C >= 1) {
+        $query .= ", ";
+      }
+      // campo nombre es el nombre de la columna de la base de datos
+      $query .= $clave['campo_nombre'] . "=" . $clave['campo_marcador'];
+      $C++;
+    }
+    $query .= " WHERE " . $condicion['condicion_campo'] .= "=" . $condicion['condicion_marcador'];
+    $sql = $this->conectar()->prepare($query);
+    foreach ($datos as $clave) {
+      // bindParam es para vincular un parametro con una variable especifica
+      $sql->bindParam($clave['campo_marcador'], $clave['campo_valor']);
+    }
+    $sql->bindParam($condicion['condicion_marcador'], $condicion['condicion_valor']);
+    $sql->execute();
+  }
+
+  protected function eliminarRegistro($tabla, $campo, $id)
+  {
+    $sql = $this->conectar()->prepare("DELETE FROM $tabla WHERE $campo = :id");
+    $sql->bindParam(":id", $id);
+    $sql->execute();
+    return $sql;
+  }
+
+  protected function paginadorTablas($pagina, $numeroPaginas, $url, $botones)
+  {
+    $tabla = '<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+    if ($pagina <= 1) {
+      $tabla .= '
+        <a class="pagination-previous is-disabled" disabled >Anterior</a>
+        <ul class="pagination-list">
+      ';
+    } else {
+      $tabla .= '
+        <a class="pagination-previous" href="' . $url . ($pagina - 1) . '/">Anterior</a>
+        <ul class="pagination-list">
+          <li><a class="pagination-link" href="' . $url . '1/">1</a></li>
+          <li><span class="pagination-ellipsis">&hellip;</span></li>
+      ';
+    }
+
+    $ci = 0;
+    for ($i = $pagina; $i <= $numeroPaginas; $i++) {
+      if ($ci >= $botones) {
+        break;
+      }
+      if ($pagina == $i) {
+        $tabla .= '<li><a class="pagination-link is-current" href="' . $url . $i . '/">' . $i . '</a></li>';
+      } else {
+        $tabla .= '<li><a class="pagination-link" href="' . $url . $i . '/">' . $i . '</a></li>';
+      }
+      $ci++;
+    }
+
+    if ($pagina == $numeroPaginas) {
+      $tabla .= '
+        </ul>
+        <a class="pagination-next is-disabled" disabled >Siguiente</a>
+      ';
+    } else {
+      $tabla .= '
+          <li><span class="pagination-ellipsis">&hellip;</span></li>
+          <li><a class="pagination-link" href="' . $url . $numeroPaginas . '/"></a></li>
+        </ul>
+        <a class="pagination-next" href="' . $url . ($pagina + 1) . '">Siguiente</a>
+      ';
+    }
+
+    $tabla .= '</nav>';
+
+    return $tabla;
   }
 }
