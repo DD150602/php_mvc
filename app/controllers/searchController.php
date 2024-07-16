dc4bc3bda1f9fd775387e1b00b46fb751b608396
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-07-16 16:06:19 -0500

    ✨ Add feature to know if a path exists

--- app/controllers/searchController.php
+++ app/controllers/searchController.php
@@ -6,4 +6,14 @@ use app\models\mainModel;
 
 class searchController extends mainModel
 {
+  public function modulosBusquedaControlador($modulo){
+    $listaModulos = ['userSearch'];
+
+    if (in_array($modulo, $listaModulos)) {
+      return false;
+    } else {
+      return true;
+    }
+    
+  }
 }
