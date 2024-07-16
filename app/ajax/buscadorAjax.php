a305d795e907a2dae6a1c59e2b317b177e77232c
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-07-16 16:01:20 -0500

    ✨ Add search feature

---
+++ app/ajax/buscadorAjax.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../../config/app.php');
+require_once('../views/inc/session_start.php');
+require_once('../../autoload.php');
+
+use app\controllers\searchController;
+
+// el 'modulo_usuario' es para saber si se esta utilizando el formulario de registrar
+if (isset($_POST['modulo_buscador'])) {
+  $insBuscador = new searchController();
+  
+
+} else {
+  // evitar que se acceda a la ruta 
+  session_destroy();
+  header("Location: " . APP_URL . "login/");
+}
---
+++ app/controllers/searchController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\mainModel;
+
+class searchController extends mainModel
+{
+}
